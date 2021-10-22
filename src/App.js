7db9d924eae5ed4b5c0757a6bff4a4fe7d2f52e6
teja219a <teja219a@gmail.com>
2021-10-22 14:58:44 +0000

    Conitional Redering using React

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+  }
+
+  renderAuthButton = () => {}
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+        {isLoggedIn && <button>Login</button>}
+        {!isLoggedIn && <button>Logout</button>}
+      </div>
+    )
+  }
+}
+
+export default App
