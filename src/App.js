a3ad1b307fdc27f0aa5dc39e666d11863551ee3a
TEJA SRI SAI ATUKURI <59640814+teja219a@users.noreply.github.com>
2021-10-22 20:35:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,3 +25,93 @@ class App extends Component {
 }
 
 export default App
+
+//-----------------------------------------Using an If...Else Statement-------------------------
+// //import { Component } from "react"
+// import './App.css'
+
+// class App extends Component {
+//   state = { isLoggedIn: true }
+
+//    renderAuthButton = () => {
+//     const {isLoggedIn} = this.state
+//     if (isLoggedIn === true) {
+//       return <button>Logout</button>
+//     }
+//     return <button>Login</button>
+//   }
+
+//   render() {
+//     return (
+//      <div className="container">
+//         {this.renderAuthButton()}
+//       </div>
+//     )
+//   }
+// }
+
+// export default App//
+
+//-----------------------------------------------Using Element Variables-----------------------------
+// import { Component } from "react"
+// import './App.css'
+
+// class App extends Component {
+//   state = { isLoggedIn: true }
+
+//   render() {
+//     const { isLoggedIn } = this.state
+//     let authButton
+//     if (isLoggedIn) {
+//       authButton = <button>Logout</button>
+//     } else {
+//       authButton = <button>Login</button>
+//     }
+//     return (
+//       <div className="container">
+//         <h1>React JS</h1>
+//         {authButton}
+//       </div>
+//     )
+//  }
+// }
+
+// export default App
+
+//----------------------------------------------------------Using Ternary Operators-------------------------------
+// import { Component } from "react"
+// import './App.css'
+
+// class App extends Component {
+
+//   render() {
+//     const { isLoggedIn } = this.state
+//     return (
+//       <div className="container">
+//         {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+//---------------------------------------------------Using Logical && Operator------------------------
+// import { Component } from "react"
+// import './App.css'
+
+// class App extends Component {
+
+//   render() {
+//     const { isLoggedIn } = this.state
+//     return (
+//       <div className="container">
+//         {isLoggedIn && <button>Logout</button>}
+//         {!isLoggedIn && <button>Login</button>}
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
